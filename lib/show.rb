112628743b46841d3829a7ec9c7d832a7df6b3d6
Scott Thomas <smthom05@gmail.com>
2018-12-12 09:18:58 -0700

    show class and tests complete

---
+++ lib/show.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Show
+
+  attr_reader :name,
+              :creator,
+              :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    total_show_salary = 0
+    characters.each do |character|
+      total_show_salary += character.salary
+    end
+    total_show_salary
+  end
+end
